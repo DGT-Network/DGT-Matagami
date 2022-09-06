af966608b0cff48971893275ff53648894a855a1
sparsov <sparsov@sinergo.ru>
2022-09-07 01:00:03 +0700

    fix for dec

---
+++ GARANASKA_BETA/families/settings/dgt_settings/processor/utils.py
@@ -0,0 +1,41 @@
+# Copyright 2019 DGT NETWORK INC © Stanislav Parsov 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+import hashlib
+import base64
+from functools import lru_cache
+
+# The config namespace is special: it is not derived from a hash.
+SETTINGS_NAMESPACE = '000000'
+
+def _to_hash(value):
+    return hashlib.sha256(value.encode()).hexdigest()
+
+
+_MAX_KEY_PARTS = 4
+_ADDRESS_PART_SIZE = 16
+_EMPTY_PART = _to_hash('')[:_ADDRESS_PART_SIZE]
+
+
+@lru_cache(maxsize=128)
+def _make_settings_key(key):
+    # split the key into 4 parts, maximum
+    key_parts = key.split('.', maxsplit=_MAX_KEY_PARTS - 1)
+    # compute the short hash of each part
+    addr_parts = [_to_hash(x)[:_ADDRESS_PART_SIZE] for x in key_parts]
+    # pad the parts with the empty hash, if needed
+    addr_parts.extend([_EMPTY_PART] * (_MAX_KEY_PARTS - len(addr_parts)))
+
+    return SETTINGS_NAMESPACE + ''.join(addr_parts)
