87b03988a5f486eab131e063badcf7c24bae9c09
sparsov <sparsov@sinergo.ru>
2022-11-10 01:30:37 +0700

    add test cmd list for notary

---
+++ GARANASKA_BETA/ntest.sh
@@ -0,0 +1,9 @@
+dgt keygen mkey --key-dir /tmp -cb openssl
+notary crt /project/dgt/etc/certificate.json --user 12345678
+notary wallet /tmp/mkey.priv --did "did:notary:30563010:12345678"
+notary wallets "did:notary:30563010:12345678"
+notary role myrole "did:notary:30563010:12345678" --keyfile /tmp/mkey.priv
+notary roles "did:notary:30563010:12345678"
+notary target mytarget "did:notary:30563010:12345678" 100 --invoice --keyfile /tmp/mkey.priv
+notary goods  "did:notary:30563010:12345678"
+
