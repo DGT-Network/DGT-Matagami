c1debec639b77bcb487dd571ccab4ca95f2e1310
sparsov <sparsov@sinergo.ru>
2023-02-27 13:29:56 +0700

    add sample for multi sign

---
+++ GARANASKA_BETA/dec_send_from_corp.sh
@@ -0,0 +1,2 @@
+dec send  /project/peer/keys/corp.pub /project/peer/keys/wkey3.pub 10 --keyfile /project/peer/keys/corp3.priv -tid CORP001
+dec send  /project/peer/keys/corp.pub /project/peer/keys/wkey3.pub 10 --keyfile /project/peer/keys/corp2.priv -tid CORP001
