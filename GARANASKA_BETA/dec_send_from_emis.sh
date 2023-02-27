82c8cd65c10339c4c3101abbe60b088585c5dc00
sparsov <sparsov@sinergo.ru>
2023-02-27 14:11:09 +0700

    add sample for multi sign from emission

---
+++ GARANASKA_BETA/dec_send_from_emis.sh
@@ -0,0 +1,2 @@
+dec send _DEC_EMISSION_KEY_ /project/peer/keys/corp.priv 10 --keyfile /project/peer/keys/corp1.priv  -tid CORP002
+dec send _DEC_EMISSION_KEY_ /project/peer/keys/corp.priv 10 --keyfile /project/peer/keys/corp2.priv  -tid CORP002
